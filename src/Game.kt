9986e2880a25163e5d6341dae142587341253271
matt <mholford@protonmail.com>
2019-12-28 07:31:38 -0800

    Functions:  Anatomy  Unit Functions  Named Function Arguments  Single-Expression Function Syntax  Default Parameters

--- src/Game.kt
+++ src/Game.kt
@@ -5,11 +5,19 @@ fun main(args: Array<String>) {
     val isImmortal = false
 
     // Aura code
-    val auraVisible = isBlessed && hp > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    val auraColor = auraColor(isBlessed, hp, isImmortal)
 
     //Status
-    val healthStatus = when (hp) {
+    val healthStatus = formatHealthStatus(hp, isBlessed)
+
+    //Output
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+
+    castFireball(5)
+}
+
+private fun formatHealthStatus(hp: Int, isBlessed: Boolean) =
+    when (hp) {
         100 -> "is in excellent condition!"
         in 75..99 -> "has a few minor cuts and bruises here and there."
         in 50..74 -> if (isBlessed) {
@@ -17,12 +25,29 @@ fun main(args: Array<String>) {
         } else {
             "has some lacerations and potentially internal bleeding."
         }
-    in 25..49 -> "is hemorrhaging blood and has multiple fractures. Seek medical attention immediately!"
-    else -> "has ruptured organs and is bordering on shock. I hope you wrote your will..."
-    }
+        in 25..49 -> "is hemorrhaging blood and has multiple fractures. Seek medical attention immediately!"
+        else -> "has ruptured organs and is bordering on shock. I hope you wrote your will..."
+}
 
-    //Output
-    println("(Aura: $auraColor) " +
-    " (Blessed: ${if (isBlessed) "YES" else "NO"})")
+private fun auraColor(isBlessed: Boolean, hp: Int, isImmortal: Boolean): String {
+    val auraVisible = isBlessed && hp > 50 || isImmortal
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    return auraColor
+}
+
+private fun printPlayerStatus(
+    auraColor: String,
+    isBlessed: Boolean,
+    name: String,
+    healthStatus: String
+) {
+    println(
+        "(Aura: $auraColor) " +
+                " (Blessed: ${if (isBlessed) "YES" else "NO"})"
+    )
     println("$name $healthStatus")
+}
+
+private fun castFireball(numFireballs: Int = 2) {
+    println("A glass of Fireball springs into existence. (x$numFireballs)")
 }
