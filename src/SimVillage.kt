af1ad7b3ec25812d6527eeca34134aec00270ce3
matt <mholford@protonmail.com>
2019-12-28 18:15:58 -0800

    ==[Exercise 5: Functions II]==   Function Types   Functions have implicit returns   'it' keyword   Type Inference   Lambdas   Inlining   Function Reference   Higher-Order Functions

---
+++ src/SimVillage.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) { runSimulation()}
+
+fun runSimulation() {
+    val greetingFunction = configureGreetingFunction()
+    println(greetingFunction("Guyal"))
+    println(greetingFunction("Guyal"))
+}
+
+fun configureGreetingFunction(): (String) -> String {
+    val structureType = "hospitals"
+    var numBuildings = 5
+    return { playerName: String ->
+        val currentYear = 2019
+        numBuildings += 1
+        println("Addings $numBuildings $structureType")
+        "Welcome to SimVillage, $playerName (copyright $currentYear)"
+    }
+}
+
+fun printConstructionCost(numBuildings: Int) {
+    val cost = 500
+    println("construction cost: ${cost * numBuildings}")
+}
