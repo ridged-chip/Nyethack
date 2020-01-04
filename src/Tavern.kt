b13b7e636878e89e99718cda0fb0a06692dfdff3
matt <mholford@protonmail.com>
2020-01-04 09:31:49 -0800

    ==[Exercise 7: Strings]== substring split string comparison

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,14 +1,34 @@
 const val TAVERN_NAME = "Taernyl's Folly"
 
 fun main(args: Array<String>) {
-    var beverage = readLine()
+    placeOrder("elixir,Shirley's Temple,4.12")
+}
 
-    if (beverage != null) {
-        beverage = beverage.capitalize()
-    } else {
-        println("I can't capitalize a null value!")
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiou]")) {
+        when (it.value) {
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
     }
 
-    val beverageServed: String = beverage ?: "Buttered Ale"
-    println(beverageServed)
+private fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their order.")
+
+    val (type, name, price) = menuData.split(',')
+    val message = "Madrigal buys a $name ($type) for $price"
+    println(message)
+
+    val phrase = if (name == "Dragon's Breath") {
+        "Madrigal exclaims: ${toDragonSpeak("Ah, delicious $name!")}"
+    } else {
+        "Madrigal says: Thanks for the $name"
+    }
+    println(phrase)
 }
